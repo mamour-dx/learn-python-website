0fa6defa73a7235682e6fedbbca8e9ac10b0df6d
Mamour Dieng <149331871+mamour-dx@users.noreply.github.com>
2024-06-11 14:05:32 +0000

    setting up quiz data

---
+++ src/assets/data.js
@@ -0,0 +1,9 @@
+export const data = [
+    {
+        question: "C'est quoi print()",
+        option1: "une fonction",
+        option2: "une méthode",
+        option3: "une classe",
+        ans : 1,
+    }
+]
