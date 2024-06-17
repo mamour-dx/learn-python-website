7fdac55695e987bbed5dba05aa99f3627c89e70b
Mamour Dieng <149331871+mamour-dx@users.noreply.github.com>
2024-06-17 00:56:49 +0000

    changing architecture of project using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.js
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import Home from './pages/Home/Home.jsx';
@@ -8,34 +9,25 @@ import Community from './pages/Community/Community.jsx';
 import SupportMe from './pages/SupportMe/SupportMe.jsx';
 import './App.css';
 
-
 const App = () => {
-  const [currentPage, setCurrentPage] = useState('Home');
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'Home':
-        return <Home />;
-      case 'Quiz':
-        return <Quiz />;
-      case 'Community':
-        return <Community />;
-      case 'SupportMe':
-        return <SupportMe />;
-      default:
-        return <Home />;
-    }
-  };
-  console.log(currentPage);
   return (
-    <div className="App">
-      <Header setCurrentPage={setCurrentPage} />
-      {renderPage()}
-    <footer className="footer">
-      <Footer />
-    </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quiz/*" element={<Quiz />} />
+            <Route path="/community" element={<Community />} />
+            <Route path="/support-me" element={<SupportMe />} />
+          </Routes>
+        </main>
+        <footer className="footer">
+          <Footer />
+        </footer>
+      </div>
+    </Router>
   );
 };
 
 export default App;
-
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
+// src/components/Header/Header.jsx
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../../assets/logo.png';
 import python from '../../assets/python.png';
@@ -7,34 +9,34 @@ import quizIcon from '../../assets/icons/quiz.png';
 import communityIcon from '../../assets/icons/community.png';
 import supportIcon from '../../assets/icons/supportMe.png';
 
-const Header = ({ setCurrentPage }) => {
-    const [isOpen, setIsOpen] = useState(false);  
-    
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header className="header">
             <div className='logo-container'>
-            <a href='https://mxr.codes' rel="noreferrer" target='_blank'><img src={logo} alt="Logo" className="logo" /></a>
+                <a href='https://mxr.codes' rel="noreferrer" target='_blank'><img src={logo} alt="Logo" className="logo" /></a>
             </div>
 
-            <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>  
+            <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
             </div>
 
-            <nav className={isOpen ? "nav-list active" : "nav-list"}>  
-                <a href='#home'><li className="nav-item" onClick={() => setCurrentPage('Home')}>
+            <nav className={isOpen ? "nav-list active" : "nav-list"}>
+                <Link to="/" onClick={() => setIsOpen(false)}><li className="nav-item">
                     <img src={homeIcon} alt="Home" className="icon" />Home
-                </li></a>
-                <a href='#quiz'><li className="nav-item" onClick={() => setCurrentPage('Quiz')}>
+                </li></Link>
+                <Link to="/quiz" onClick={() => setIsOpen(false)}><li className="nav-item">
                     <img src={quizIcon} alt="Quiz" className="icon" />Quiz
-                </li></a>
-                <a href='#community'><li className="nav-item" onClick={() => setCurrentPage('Community')}>
+                </li></Link>
+                <Link to="/community" onClick={() => setIsOpen(false)}><li className="nav-item">
                     <img src={communityIcon} alt="Community" className="icon" />Community
-                </li></a>
-                <a href="#support-me"><li className="nav-item" onClick={() => setCurrentPage('SupportMe')}>
+                </li></Link>
+                <Link to="/support-me" onClick={() => setIsOpen(false)}><li className="nav-item">
                     <img src={supportIcon} alt="Support" className="icon" />Support Me
-                </li></a>
+                </li></Link>
             </nav>
             <img src={python} alt="Python" className="python"/>
         </header>
