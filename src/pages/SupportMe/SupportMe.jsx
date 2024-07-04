1554272bef09aa8c83a59fa1f580eb0dcf4fb862
Mamour Dieng <149331871+mamour-dx@users.noreply.github.com>
2024-07-04 13:45:21 +0000

    share it with friend

--- src/pages/SupportMe/SupportMe.jsx
+++ src/pages/SupportMe/SupportMe.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './SupportMe.css';
+
 const SupportMe = () => {
   return (
     <div className='support-me'>
       <h1 className='glass'>Merci d'avoir utilisé mon application⭐️</h1>
       <div className='support-content'>
         <div className='Share'>
-          <h2>Partages</h2>
+          <h2 className='share'>Partages à un ami</h2>
+          <h3>Envoie le site à un ami qui pourrait être intéressé par apprendre python ?</h3>
+          <img src="src/assets/share.svg" alt="share" />
+        
+          
         </div>
         <div className='donate'>
           <h2 className='donation-cta'>Vous pouvez aussi faire un <a href='https://ko-fi.com/mxrcodes' className='kofi-profile' target='_blank' rel="noopener noreferrer">don</a></h2>
