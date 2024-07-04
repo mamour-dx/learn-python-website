342807dbe456e25268a9f2be4324d36bb110aaf7
Mamour Dieng <149331871+mamour-dx@users.noreply.github.com>
2024-07-04 00:31:32 +0000

    supportme, first commit

--- src/pages/SupportMe/SupportMe.jsx
+++ src/pages/SupportMe/SupportMe.jsx
@@ -1,8 +1,18 @@
 // src/pages/Contact.js
 import React from 'react';
+import './SupportMe.css';
 
 const SupportMe = () => {
-  return <div>Welcome to the support me page</div>;
+  return <div>
+  <h1>Merci d'avoir utilisé mon site web</h1>
+  <iframe
+  id="kofiframe"
+  src="https://ko-fi.com/mxrcodes/?hidefeed=true&widget=true&embed=true&preview=true"
+  title="mxrcodes"
+  height="712"
+/>
+<p className='avis'>Pour toute suggestion ou idée d'amélioration, écris-moi sur <a href="mailto:contact@mxr.codes">contact@mxr.codes</a></p>
+</div>;
 };
 
 export default SupportMe;
